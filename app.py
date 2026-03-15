3e5373e6707fd7c9fbbf05b596eb721d4b04476a
HomnadhHaigreevaKumarPuppala-5607 <puppalahaigreevakumar@gmail.com>
2026-03-15 15:36:55 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request
+import joblib
+import pandas as pd
+
+app = Flask(__name__)
+
+# Load trained model
+model = joblib.load("house_price_model.pkl")
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/predict", methods=["POST"])
+def predict():
+
+    area = float(request.form["area"])
+    bedrooms = int(request.form["bedrooms"])
+    bathrooms = int(request.form["bathrooms"])
+    stories = int(request.form["stories"])
+    parking = int(request.form["parking"])
+
+    input_data = pd.DataFrame(
+        [[area, bedrooms, bathrooms, stories, parking]],
+        columns=['area','bedrooms','bathrooms','stories','parking']
+    )
+
+    prediction = model.predict(input_data)[0]
+
+    return render_template(
+        "index.html",
+        prediction_text=f"Predicted House Price: ₹ {round(prediction,2)}"
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True)
